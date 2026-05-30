bc2a0a8907e1c52761b41d50f069e8fc061088d3
fmarcelomorais <marcelomorais.program@gmail.com>
2026-05-30 10:26:58 -0300

    commit inicial  - pwa

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker ativado');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // por enquanto não faz cache
+});
